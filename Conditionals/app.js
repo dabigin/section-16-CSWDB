8acc78002726ed69b181b22dd5999334961f492e
Brandon Dalton <dabigin@gmail.com>
2022-10-26 13:14:37 -0500

    Section 16 Javascript Decision Making Lesson 167 Else

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -1,22 +1,21 @@
 // let random = Math.random();
 // if (random < 0.5) {
 //   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-//   console.log(random);
+// } else {
+//   console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!");
 // }
+// console.log(random);
 
-// if (random >= 0.5) {
-//   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-//   console.log(random);
-// }
+// const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
 
-// const dayOfWeek = "Monday";
-
-// if (dayOfWeek === "Monday") {
+// if (dayOfWeek === "monday") {
 //   console.log("UGHHH I HATE MONDAYS!");
-// } else if (dayOfWeek === "Saturday") {
+// } else if (dayOfWeek === "saturday") {
 //   console.log("YAY I LOVE SATURDAYS!");
-// } else if (dayOfWeek === "Friday") {
+// } else if (dayOfWeek === "friday") {
 //   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//   console.log("MEH");
 // }
 
 // General Admission
@@ -25,7 +24,7 @@
 // 10-65 ADULT $20
 // 65+ SENIOR $10
 
-const age = 8;
+const age = 89;
 
 if (age < 5) {
   console.log("You are a baby. You get in for free");
@@ -33,6 +32,6 @@ if (age < 5) {
   console.log("You are a child, you get in for $10");
 } else if (age < 65) {
   console.log("You are an adult. You pay $20");
-} else if (age > 65) {
+} else {
   console.log("You are a Senior. You Pay $10");
 }
