084a379f6a1a517f1b39e3bfb262c42bf9396b28
Brandon Dalton <dabigin@gmail.com>
2022-10-26 12:11:47 -0500

    Section 16 Lesson 166 Else If

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -1,10 +1,38 @@
-let random = Math.random();
-if (random < 0.5) {
-  console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-  console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//   console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//   console.log(random);
+// }
 
-if (random >= 0.5) {
-  console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-  console.log(random);
+// if (random >= 0.5) {
+//   console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//   console.log(random);
+// }
+
+// const dayOfWeek = "Monday";
+
+// if (dayOfWeek === "Monday") {
+//   console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === "Saturday") {
+//   console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === "Friday") {
+//   console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// }
+
+// General Admission
+// 0-5 FREE
+// 5-10 CHILD $10
+// 10-65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8;
+
+if (age < 5) {
+  console.log("You are a baby. You get in for free");
+} else if (age < 10) {
+  console.log("You are a child, you get in for $10");
+} else if (age < 65) {
+  console.log("You are an adult. You pay $20");
+} else if (age > 65) {
+  console.log("You are a Senior. You Pay $10");
 }
