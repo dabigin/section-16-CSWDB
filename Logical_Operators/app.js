31fb6e68e625a87c9d7ce12225318514314d3aa3
Brandon Dalton <dabigin@gmail.com>
2022-10-27 22:18:21 -0500

    Section 16 Lesson NOT Operator

--- Logical_Operators/app.js
+++ Logical_Operators/app.js
@@ -10,13 +10,23 @@
 // 10-65 $20
 // 65+ free
 
-const age = 100;
-if ((age >= 0 && age < 5) || age >= 65) {
-  console.log("FREE");
-} else if (age >= 5 && age < 10) {
-  console.log("$10");
-} else if (age >= 10 && age < 65) {
-  console.log("$20");
-} else {
-  console.log("INVALID AGE!");
+// const age = 100;
+// if ((age >= 0 && age < 5) || age >= 65) {
+//   console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//   console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//   console.log("$20");
+// } else {
+//   console.log("INVALID AGE!");
+// }
+
+// const firstName = prompt("Enter your first name");
+// if (!firstName) {
+//   prompt("TRY AGAIN!!!");
+// }
+
+const age = 8;
+if (!((age >= 0 && age < 5) || age >= 65)) {
+  console.log("YOU ARE NOT A BABY OR A SENIOR!");
 }
